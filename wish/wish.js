252ad1fa4ed0693af8f5386b7853dddc0a200a93
JIE201027 <asd2324192017@gmail.com>
2026-03-07 13:56:21 +0800

    chore: 強制將資料夾結構同步為最新的 wish 結構

--- wish/wish.js
+++ wish/wish.js
@@ -31,7 +31,7 @@ window.addEventListener('scroll', function () {
 
 document.addEventListener("DOMContentLoaded", function () {
     // 加上最前面的斜槓 /，這是最穩定的寫法
-    fetch('/single-page-website/single-data.json?t=' + new Date().getTime())
+    fetch('/wish/single-data.json?t=' + new Date().getTime())
         .then(res => res.json())
         .then(data => {
             // 1. 更新 Banner
